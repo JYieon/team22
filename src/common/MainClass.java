6f2bd50cf7872fc1e935545b11529345c20d394d
ChoiJiyeon <dkcl.8642@gmail.com>
2024-10-16 14:48:41 +0900

    team2

---
+++ src/common/MainClass.java
@@ -0,0 +1,30 @@
+package common;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println("1번 ");
+			System.out.println("2번 ");
+			System.out.println("3번 ");
+			System.out.println("4번 ");
+			System.out.println("5번 ");
+			num = input.nextInt();
+			switch(num) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			}
+		}
+	}
+}
