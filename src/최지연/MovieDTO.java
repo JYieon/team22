b948eae31b26b7fec77cca38d5af3ce7fc32cd71
ChoiJiyeon <dkcl.8642@gmail.com>
2024-10-18 09:13:02 +0900

    영화옵션 추가

--- src/최지연/MovieDTO.java
+++ src/최지연/MovieDTO.java
@@ -1,19 +1,52 @@
 package 최지연;
 
-import java.util.ArrayList;
 
-public class MovieDTO { //10월 16일 기준 영화
-	private ArrayList<String> movie = new ArrayList<String>();
-
-	public void MovieDTO() {}
-	public void MovieDTO(ArrayList<String> movie) {}
-	
-	public ArrayList<String> getMovie() {
-		return movie;
-	}
-
-	public void setMovie(String movie) {
-		this.movie.add(movie);
-	}
-	
+public class MovieDTO { 
+   private String uid, cinema, day, time, name;
+   private int peop;
+   private int movieCount = 0;
+   
+   public String getUid() {
+      return uid;
+   }
+   public void setUid(String uid) {
+      this.uid = uid;
+   }
+   public String getCinema() {
+      return cinema;
+   }
+   public void setCinema(String cinema) {
+      this.cinema = cinema;
+   }
+   public String getDay() {
+      return day;
+   }
+   public void setDay(String day) {
+      this.day = day;
+   }
+   public String getTime() {
+      return time;
+   }
+   public void setTime(String time) {
+      this.time = time;
+   }
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public int getPeop() {
+      return peop;
+   }
+   public void setPeop(int peop) {
+      this.peop = peop;
+   }
+   public int getMovieCount() {
+      return movieCount;
+   }
+   public void setMovieCount(int movieCount) {
+      this.movieCount = movieCount;
+   }
+   
 }
